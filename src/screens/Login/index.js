601a4ec8799a0fa1e972425a622a6f12ef97c6f8
Abhishek Gupta <a@navgurukul.org>
2020-05-25 01:47:32 +0530

    minor mistake

--- src/screens/Login/index.js
+++ src/screens/Login/index.js
@@ -1,4 +1,4 @@
-/import React from 'react';
+import React from 'react';
 import GoogleLogin from "react-google-login";
 import Card from '@material-ui/core/Card';
 import Grid from '@material-ui/core/Grid';
